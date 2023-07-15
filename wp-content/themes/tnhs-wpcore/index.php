2885dac14d86a6ac6eda63f2130400f9f32f596a
mrjerox <nghiatrangbuonkhivangbongem@gmail.com>
2023-07-15 22:32:28 +0700

    reup

---
+++ wp-content/themes/tnhs-wpcore/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package tnhs-wpcore
+ */
+
+get_header();
+?>
+	<main id="primary" class="site-main">
+       <h1>Main asdjaskldj las w  </h1> 
+	</main>
+<?php
+get_footer();
