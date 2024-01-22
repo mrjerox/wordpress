4c7bfd9d5e370e8e754556ae1244a33d61668a15
mrjerox <nghiatrangbuonkhivangbongem@gmail.com>
2024-01-22 18:41:10 +0700

    fix breadcrumb

--- wp-content/themes/tnhs-wpcore/inc/Function.php
+++ wp-content/themes/tnhs-wpcore/inc/Function.php
@@ -5,13 +5,13 @@ function custom_breadcrumb()
     $here_text        = __('');
     $home_link        = home_url('/');
     $home_text        = __('Trang chủ');
-    $link_before      = '<span typeof="v:Breadcrumb">';
-    $link_after       = '</span>';
-    $link_attr        = ' rel="v:url" property="v:title"';
+    $link_before      = '<li>';
+    $link_after       = '</li>';
+    $link_attr        = '';
     $link             = $link_before . '<a' . $link_attr . ' href="%1$s">%2$s</a>' . $link_after;
-    $delimiter        = ' / ';              // Delimiter between crumbs
-    $before           = '<span class="current">'; // Tag before the current crumb
-    $after            = '</span>';                // Tag after the current crumb
+    $delimiter        = '&nbsp;/&nbsp;';              // Delimiter between crumbs
+    $before           = '<li class="current">'; // Tag before the current crumb
+    $after            = '</li>';                // Tag after the current crumb
     $page_addon       = '';                       // Adds the page number if the query is paged
     $breadcrumb_trail = '';
     $category_links   = '';
@@ -183,7 +183,7 @@ function custom_breadcrumb()
     }
 
     $breadcrumb_output_link  = '';
-    $breadcrumb_output_link .= '<div class="breadcrumb mb-6 text-xs font-medium uppercase">';
+    $breadcrumb_output_link .= '<ol class="flex items-center flex-wrap breadcrumb mb-6 text-xs font-medium uppercase">';
     if (
         is_home()
         || is_front_page()
@@ -191,17 +191,17 @@ function custom_breadcrumb()
         // Do not show breadcrumbs on page one of home and frontpage
         if (is_paged()) {
             // $breadcrumb_output_link .= $here_text . $delimiter;
-            $breadcrumb_output_link .= '<a class="text-xs font-medium uppercase text-neutral-400" href="' . $home_link . '">' . $home_text . '</a>';
+            $breadcrumb_output_link .= '<li><a class="text-xs font-medium uppercase text-neutral-400" href="' . $home_link . '">' . $home_text . '</a></li>';
             $breadcrumb_output_link .= $page_addon;
         }
     } else {
         // $breadcrumb_output_link .= $here_text . $delimiter;
-        $breadcrumb_output_link .= '<a class="text-xs font-medium uppercase text-neutral-400" href="' . $home_link . '">' . $home_text . '</a>';
+        $breadcrumb_output_link .= '<li><a class="text-xs font-medium uppercase text-neutral-400" href="' . $home_link . '">' . $home_text . '</a></li>';
         $breadcrumb_output_link .= $delimiter;
         $breadcrumb_output_link .= $breadcrumb_trail;
         $breadcrumb_output_link .= $page_addon;
     }
-    $breadcrumb_output_link .= '</div><!-- .breadcrumbs -->';
+    $breadcrumb_output_link .= '</ol><!-- .breadcrumbs -->';
 
     return $breadcrumb_output_link;
 }
